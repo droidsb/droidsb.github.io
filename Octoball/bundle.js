61ea1c5f94f975fb09974ac3433dabf1c90a3137
Seth Baird <droidsb@users.noreply.github.com>
2020-03-03 23:08:06 -0700

    ignore

--- Octoball/bundle.js
+++ Octoball/bundle.js
@@ -60373,13 +60373,159 @@ for(var i=0; i<data.length; i++){
 
 //document.body.appendChild(document.createElement("br"));
 
-addText(document.getElementById("stats"),"Name:"+data[i].name+"<br>Kills:"+data[i].kills+"<br>Deaths:"+data[i].deaths+"<br>Self Deaths:"+data[i].sdeaths+"<br>Wins:"+data[i].wins+"<br>Losses:"+data[i].losses+"<br>WLR:"+data[i].wlr+"<br>KDR:"+data[i].kdr+"<br>KDR(Self Deaths):"+data[i].kdrsd+"<br><br>");
+//addText(document.getElementById("stats"),"Name:"+data[i].name+"<br>Kills:"+data[i].kills+"<br>Deaths:"+data[i].deaths+"<br>Self Deaths:"+data[i].sdeaths+"<br>Wins:"+data[i].wins+"<br>Losses:"+data[i].losses+"<br>WLR:"+data[i].wlr+"<br>KDR:"+data[i].kdr+"<br>KDR(Self Deaths):"+data[i].kdrsd+"<br><br>");
 
 
 
 }
 
 
+{
+var topplayer;
+
+var secondplayer;
+
+var thirdplayer;
+
+for(var i=0; i<data.length; i++){
+	
+	if(i===0){
+	
+		topplayer=data[i]
+		
+	}
+	
+	if(data[i].wins>topplayer.wins){
+	
+		topplayer=data[i];
+	
+	}
+
+
+}
+
+for(var i=0; i<data.length; i++){
+	
+	if(i===0){
+	
+		secondplayer={wins:-100}
+		
+	}
+	
+	if(data[i].wins>secondplayer.wins && data[i].wins<topplayer.wins){
+	
+		secondplayer=data[i];
+	
+	}
+
+
+}
+
+for(var i=0; i<data.length; i++){
+	
+	if(i===0){
+	
+		thirdplayer={wins:-100}
+		
+	}
+	
+	if(data[i].wins>thirdplayer.wins && data[i].wins<secondplayer.wins){
+	
+		thirdplayer=data[i];
+	
+	}
+
+
+}
+
+}
+
+{
+var onewr;
+
+var twowr;
+
+var threewr;
+
+for(var i=0; i<data.length; i++){
+	
+	if(i===0){
+	
+		onewr=data[i]
+		
+	}
+	
+	if(data[i].wlr>onewr.wlr){
+	
+		onewr=data[i];
+	
+	}
+
+
+}
+
+for(var i=0; i<data.length; i++){
+	
+	if(i===0){
+	
+		twowr={wlr:-100}
+		
+	}
+	
+	if(data[i].wlr>twowr.wlr && data[i].wlr<onewr.wlr){
+	
+		twowr=data[i];
+	
+	}
+
+
+}
+
+for(var i=0; i<data.length; i++){
+	
+	if(i===0){
+	
+		threewr={wlr:-100}
+		
+	}
+	
+	if(data[i].wlr>threewr.wlr && data[i].wlr<twowr.wlr){
+	
+		threewr=data[i];
+	
+	}
+
+
+}
+
+}
+
+console.log(topplayer)
+addText(document.getElementById("leaderboard"),"Wins Leaderboard<br><br>1st: "+topplayer.name+"<br>Wins: "+topplayer.wins+"<br><br>2nd: "+secondplayer.name+"<br>Wins: "+secondplayer.wins+"<br><br>3rd: "+thirdplayer.name+"<br>Wins: "+thirdplayer.wins+"<br><br>")
+
+addText(document.getElementById("leaderboard2"),"Win Ratio Leaderboard<br><br>1st: "+onewr.name+"<br>Win Ratio: "+onewr.wlr+"<br><br>2nd: "+twowr.name+"<br>Win Ratio: "+twowr.wlr+"<br><br>3rd: "+threewr.name+"<br>Win Ratio: "+threewr.wlr+"<br><br>")
+
+
+
+
+
+function getPlayer(){
+
+var x = document.getElementById("myText").value;
+
+	var p=searchname(x, data)
+	
+	document.getElementById("stats").innerHTML = "";
+	
+	//document.getElementById("stats").removeChild()
+	
+	addText(document.getElementById("stats"),"Name:"+p.name+"<br>Kills:"+p.kills+"<br>Deaths:"+p.deaths+"<br>Self Deaths:"+p.sdeaths+"<br>Wins:"+p.wins+"<br>Losses:"+p.losses+"<br>WLR:"+p.wlr+"<br>KDR:"+p.kdr+"<br>KDR(Self Deaths):"+p.kdrsd+"<br><br>");
+
+
+
+}
+
+document.getElementById("button").onclick = function() {getPlayer()};
 
 /*
 document.writeln(name+"<br>")
